7e252a939bd6770127f8cf50c3e824513e49ea55
wwx <w17731138318@qq.com>
2018-01-08 11:14:51 +0800

    ~

--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,41 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ],
-      }, 
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader?indentedSyntax'
+        ],
+      },
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
           loaders: {
-            test: /\.(eot|svg|ttf|woff|woff2)$/,
-            loader: 'file-loader'
+            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // the "scss" and "sass" values for the lang attribute to the right configs here.
+            // other preprocessors should work out of the box, no loader config like this necessary.
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+            'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
           }
           // other vue-loader options go here
         }
@@ -68,7 +95,7 @@ module.exports = {
       'store': path.resolve('src/store'),
       'router': path.resolve('src/router')
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ['*', '.js', '.vue', '.json','.jsx','.css','.scss']
   },
   devServer: {
     historyApiFallback: true,
